125723b5580a04970ecb1b107a6dbd066e31106b
Dnts77 <daniel.souzadantas07@gmail.com>
2026-02-06 18:44:16 -0300

    feat: Implementação do método de atualizar

--- lib/home.dart
+++ lib/home.dart
@@ -18,13 +18,24 @@ class _HomeState extends State<Home> {
   final _db = AnotacaoHelper();
   List<Anotacao> _anotacoes = [];
 
- _exibirTelaCadastro(){
- 
+ _exibirTelaCadastro( {Anotacao? anotacao} ){
+
+  String textoSalvarAtualizar = "";
+  if(anotacao == null){
+    _tituloController.text = "";
+    _descricaoController.text = "";
+    textoSalvarAtualizar = "Salvar";
+  }else{
+    _tituloController.text = anotacao.titulo;
+    _descricaoController.text = anotacao.descricao;
+    textoSalvarAtualizar = "Atualizar";
+  }
+
   showDialog(
     context: context, 
     builder: (context){
       return AlertDialog(
-        title: Text("Adicionar anotação"),
+        title: Text("$textoSalvarAtualizar anotação"),
         content: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
@@ -66,10 +77,10 @@ class _HomeState extends State<Home> {
             onPressed: (){
 
               //método de salvar
-              _salvarAnotacao();
+              _salvarAtualizarAnotacao(anotacaoSelecionada: anotacao);
               Navigator.pop(context);
             }, 
-            child: Text("Salvar")
+            child: Text(textoSalvarAtualizar)
           )
         ],
       );
@@ -96,14 +107,21 @@ class _HomeState extends State<Home> {
   //print("anotações recuperadas: ${anotacoesRecuperadas.toString()}");
  }
 
- _salvarAnotacao() async {
+ _salvarAtualizarAnotacao( {Anotacao? anotacaoSelecionada} ) async {
   String titulo = _tituloController.text;
   String descricao = _descricaoController.text;
   
-  //print("data atual: " + DateTime.now().toString());
-  Anotacao anotacao = Anotacao(titulo, descricao, DateTime.now().toString());
-  int result = await _db.salvarAnotacao(anotacao);
-  //print("salvar anotacao: ${result.toString()}");
+  if(anotacaoSelecionada == null){
+    Anotacao anotacao = Anotacao(titulo, descricao, DateTime.now().toString());
+    int result = await _db.salvarAnotacao(anotacao);
+  }else{
+    anotacaoSelecionada.titulo = titulo;
+    anotacaoSelecionada.descricao = descricao;
+    anotacaoSelecionada.data = DateTime.now().toString();
+    int qtdAtualizada = await _db.atualizarNota(anotacaoSelecionada);
+  }
+  
+  
   _tituloController.clear();
   _descricaoController.clear();
 
@@ -161,6 +179,28 @@ class _HomeState extends State<Home> {
                   child: ListTile(
                     title: Text(anotacao.titulo),
                     subtitle: Text("${_formatarData(anotacao.data)} - ${anotacao.descricao}"),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        GestureDetector(
+                          onTap: (){
+                            _exibirTelaCadastro(anotacao: anotacao);
+                          },
+                          child: Padding(
+                            padding: EdgeInsets.only(right: 16),
+                            child: Icon(Icons.edit, color: Colors.green,),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: (){},
+                          child: Padding(
+                            padding: EdgeInsets.only(right: 0),
+                            child: Icon(Icons.remove_circle, color: Colors.red,),
+                          ),
+                        ),
+                        
+                      ],
+                    ),
                   ),
                 );
               },
